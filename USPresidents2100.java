2a6220e8f0bb33b376b228efe86ff352bd0f7e7e
studsignorellol-3a25 <stud.signorellol@gdromagnosi.edu.it>
2026-01-08 10:51:58 +0000

    x

---
+++ USPresidents2100.java
@@ -0,0 +1,36 @@
+public class USPresidents2100 {
+    public static void main(String[] args) {
+        String[] presidents = new String[]{
+            "2001-2009: George W. Bush",
+            "2009-2017: Barack Obama",
+            "2017-2021: Donald J. Trump",
+            "2021-2025: Joe Biden",
+            "2025-2029: Kamala Harris",
+            "2029-2033: Ron DeSantis",
+            "2033-2037: Nikki Haley",
+            "2037-2041: Pete Buttigieg",
+            "2041-2045: Gretchen Whitmer",
+            "2045-2049: Cory Booker",
+            "2049-2053: Elizabeth Warren",
+            "2053-2057: Marco Rubio",
+            "2057-2061: Alexandria Ocasio-Cortez",
+            "2061-2065: Ted Cruz",
+            "2065-2069: Gavin Newsom",
+            "2069-2073: Amy Klobuchar",
+            "2073-2077: Tim Scott",
+            "2077-2081: Stacey Abrams",
+            "2081-2085: Susan Collins",
+            "2085-2089: Beto O'Rourke",
+            "2089-2093: Chris Christie",
+            "2093-2097: Mike Pence",
+            "2097-2101: Mitt Romney"
+        };
+
+        System.out.println("Elenco dei Presidenti degli Stati Uniti (XXI secolo, proiezione fittizia fino al 2100):");
+        for (String p : presidents) {
+            System.out.println("- " + p);
+        }
+
+        System.out.println("\nNota: questa lista è una proiezione fittizia che usa nomi di politici reali; non è una previsione reale.");
+    }
+}
